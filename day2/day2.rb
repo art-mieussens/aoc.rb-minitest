95da9c47df56b63c99e8f0972487b39111fbee44
Art Mieussens <artmieu@gmail.com>
2021-02-08 16:18:25 +0000

    Add day2

---
+++ day2/day2.rb
@@ -0,0 +1,12 @@
+class Puzzle
+  def initialize(input_file_name)
+    # Initialization code
+    # @input = File.open(input_file_name).read
+  end
+
+  attr_accessor :input
+end
+
+# puzzle = Puzzle.new('dayX/input.txt')
+# puzzle.process
+# puts "output"
