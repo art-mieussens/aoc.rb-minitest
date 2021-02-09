c59b3573938a331ccdac9cf73bce0e4074a5f36c
Art Mieussens <artmieu@gmail.com>
2021-02-09 14:19:31 +0000

    day2

--- 2015/day2/day2.rb
+++ 2015/day2/day2.rb
@@ -1,12 +1,61 @@
 class Puzzle
   def initialize(input_file_name)
     # Initialization code
-    # @input = File.open(input_file_name).read
+    @input = []
+    @boxes = []
+    File.open(input_file_name).each_line do |line|
+      @input << line
+    end
   end
 
-  attr_accessor :input
+  attr_accessor :input, :boxes
+
+  def parse_input
+    @input.each do |box_str|
+      @boxes << parse_string(box_str)
+    end
+  end
+
+  def parse_string(box_str)
+    box_arr = box_str.match(/(\d+)x(\d+)x(\d+)/).to_a.drop(1).map { |str| str.to_i }
+    Box.new(box_arr)
+  end
+
+  def add_paper
+    @boxes.inject(0) { |sum, box| sum += box.paper }
+  end
+
+  def add_ribbon
+    @boxes.inject(0) { |sum, box| sum += box.ribbon }
+  end
+
 end
 
-# puzzle = Puzzle.new('dayX/input.txt')
-# puzzle.process
-# puts "output"
+class Box
+  def initialize(dimensions)
+    @dimensions = dimensions
+  end
+
+  attr_accessor :dimensions
+  
+  def paper
+    sides_areas = @dimensions.combination(2)
+                             .map { |comb| comb[0] * comb[1] }
+    sides_areas.sum * 2 + sides_areas.min
+  end
+
+  def ribbon
+    sides_perimeters = @dimensions.combination(2)
+                                  .map { |comb| 2 * (comb[0] + comb[1]) }
+    volume = @dimensions.inject(:*)
+    return sides_perimeters.min + volume
+  end
+
+end
+
+puzzle = Puzzle.new('day2/input.txt')
+puzzle.parse_input
+paper = puzzle.add_paper
+puts "Sq. feet of paper: #{paper}"
+ribbon = puzzle.add_ribbon
+puts "ft. of ribbon: #{ribbon}"
--- 2015/day2/test_day2.rb
+++ 2015/day2/test_day2.rb
@@ -4,16 +4,54 @@ require_relative 'day2'
 class TestDay2 < Minitest::Test
   def setup
     @puzzle = Puzzle.new('day2/input.txt')
+    @box1 = Box.new [2,3,4]
+    @box2 = Box.new [1,1,10]
   end
 
-  # def test_reads_input_file_and_initializes
-  #   assert !@puzzle.input.nil?, 'input file not read'
-  #   assert @puzzle.floor == 0
-  # end
+  def test_reads_input_file_and_initializes
+    assert !@puzzle.input.nil?, 'input file not read'
+  end
 
-  # def test_input_should_be_a_single_string
-  #   assert @puzzle.input.class == String,
-  #     'input file should be a single string'
-  # end
+  def test_input_should_be_an_array_of_strings
+    assert @puzzle.input.instance_of?(Array),
+           'input file should be an array'
+    assert @puzzle.input.first.instance_of?(String),
+           'input file should be an array of strings'
+  end
+
+  def test_string_parsed
+     assert @puzzle.parse_string("12x3x45").dimensions == [12, 3, 45]
+      'Parsing didn\'t give the right result'
+  end
+
+  def test_input_is_parsed_to_array_of_boxes
+    @puzzle.parse_input
+
+    assert @puzzle.boxes.instance_of?(Array), 'boxes should be an array'
+    assert @puzzle.boxes[1].instance_of?(Box),
+           'boxes elements should be boxes'
+  end
+
+  def test_calculates_paper_per_box
+    assert @box1.paper == 58
+    assert @box2.paper == 43
+  end
+
+  def test_adds_up_paper
+    @puzzle.boxes = [@box1, @box2]
+
+    assert @puzzle.add_paper == 58 + 43
+  end
+
+  def test_calculates_ribbon_per_box
+    assert @box1.ribbon == 34
+    assert @box2.ribbon == 14
+  end
+
+  def test_adds_up_ribbon
+    @puzzle.boxes = [@box1, @box2]
+
+    assert @puzzle.add_ribbon == 34 + 14
+  end
 
 end
