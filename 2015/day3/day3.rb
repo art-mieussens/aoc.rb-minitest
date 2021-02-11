5e1fa9bf06259cdad189221febd0e01831320c4b
Art Mieussens <artmieu@gmail.com>
2021-02-11 10:48:22 +0000

    day3

--- 2015/day3/day3.rb
+++ 2015/day3/day3.rb
@@ -1,12 +1,96 @@
 class Puzzle
   def initialize(input_file_name)
     # Initialization code
-    # @input = File.open(input_file_name).read
+    @input = File.open(input_file_name).read
+    @houses_visited = []
+    @houses_visited_by_santa = []
+    @houses_visited_by_robot = []
   end
 
-  attr_accessor :input
+  attr_accessor :input,
+                :houses_visited,
+                :houses_visited_by_santa,
+                :houses_visited_by_robot
+
+  def houses_visited_count
+    @houses_visited.uniq.length
+  end
+
+  def travel
+    current_house = [0, 0]
+    @houses_visited << current_house
+    input.split('').each do |instruction|
+      case instruction
+      when '^'
+        current_house = [ houses_visited.last[0], houses_visited.last[1] + 1 ]
+        @houses_visited << current_house
+      when 'v'
+        current_house = [ houses_visited.last[0], houses_visited.last[1] - 1 ]
+        @houses_visited << current_house
+      when '>'
+        current_house = [ houses_visited.last[0] + 1, houses_visited.last[1] ]
+        @houses_visited << current_house
+      when '<'
+        current_house = [ houses_visited.last[0] - 1 , houses_visited.last[1] ]
+        @houses_visited << current_house
+      end
+    end
+  end
+
+  def travel_both
+    current_house_santa = [0, 0]
+    current_house_robot = [0, 0]
+    @houses_visited_by_santa << current_house_santa
+    @houses_visited_by_robot << current_house_robot
+    turn = 'santa'
+
+    input.split('').each do |instruction|
+
+      if turn == 'santa'
+        case instruction
+        when '^'
+          current_house_santa = [ houses_visited_by_santa.last[0], houses_visited_by_santa.last[1] + 1 ]
+          @houses_visited_by_santa << current_house_santa
+        when 'v'
+          current_house_santa = [ houses_visited_by_santa.last[0], houses_visited_by_santa.last[1] - 1 ]
+          @houses_visited_by_santa << current_house_santa
+        when '>'
+          current_house_santa = [ houses_visited_by_santa.last[0] + 1, houses_visited_by_santa.last[1] ]
+          @houses_visited_by_santa << current_house_santa
+        when '<'
+          current_house_santa = [ houses_visited_by_santa.last[0] - 1 , houses_visited_by_santa.last[1] ]
+          @houses_visited_by_santa << current_house_santa
+        end
+        turn = 'robot'
+      else
+        case instruction
+        when '^'
+          current_house_robot = [ houses_visited_by_robot.last[0], houses_visited_by_robot.last[1] + 1 ]
+          @houses_visited_by_robot << current_house_robot
+        when 'v'
+          current_house_robot = [ houses_visited_by_robot.last[0], houses_visited_by_robot.last[1] - 1 ]
+          @houses_visited_by_robot << current_house_robot
+        when '>'
+          current_house_robot = [ houses_visited_by_robot.last[0] + 1, houses_visited_by_robot.last[1] ]
+          @houses_visited_by_robot << current_house_robot
+        when '<'
+          current_house_robot = [ houses_visited_by_robot.last[0] - 1 , houses_visited_by_robot.last[1] ]
+          @houses_visited_by_robot << current_house_robot
+        end
+        turn = 'santa'
+      end
+
+    end
+
+  end
+
+  def houses_visited_by_both_count
+    (@houses_visited_by_santa + @houses_visited_by_robot).uniq.length
+  end
 end
 
-# puzzle = Puzzle.new('day3/input.txt')
-# puzzle.process
-# puts "output"
+puzzle = Puzzle.new('day3/input.txt')
+puzzle.travel
+puts "Santa visited #{puzzle.houses_visited_count} different houses."
+puzzle.travel_both
+puts "Santa and the robot together visited #{puzzle.houses_visited_by_both_count} different houses."
--- 2015/day3/test_day3.rb
+++ 2015/day3/test_day3.rb
@@ -1,19 +1,174 @@
 require 'minitest/autorun'
-require_relative day3
+require_relative 'day3'
 
 class TestDay3 < Minitest::Test
   def setup
     @puzzle = Puzzle.new('day3/input.txt')
   end
 
-  # def test_reads_input_file_and_initializes
-  #   assert !@puzzle.input.nil?, 'input file not read'
-  #   assert @puzzle.floor == 0
-  # end
+  def test_reads_input_file_and_initializes
+    assert !@puzzle.input.nil?, 'input file not read'
+  end
 
-  # def test_input_should_be_a_single_string
-  #   assert @puzzle.input.class == String,
-  #     'input file should be a single string'
-  # end
+  def test_input_should_be_a_single_string
+    assert @puzzle.input.class == String,
+      'input file should be a single string'
+  end
+
+  def test_counts_houses_visited
+    # case 1
+    @puzzle.input = '>'
+    @puzzle.houses_visited = []
+
+    @puzzle.travel
+
+    assert_equal 2, @puzzle.houses_visited_count, 'Wrong output'
+
+    # case 2
+    @puzzle.input = '^>v<'
+    @puzzle.houses_visited = []
+
+    @puzzle.travel
+
+    assert_equal 4, @puzzle.houses_visited_count, 'Wrong output'
+
+    # case 3 
+    @puzzle.input = '^v^v^v^v^v'
+    @puzzle.houses_visited = []
+
+    @puzzle.travel
+
+    assert_equal 2, @puzzle.houses_visited_count, 'Wrong output'
+  end
+
+  def test_travel_parses_instructions
+    # case 1
+    @puzzle.input = '>'
+    @puzzle.houses_visited = []
+    expected = [[0, 0],
+                [1 ,0]]
+
+    @puzzle.travel
+    assert_equal expected, @puzzle.houses_visited,
+                'Instructions incorrectly parsed'
+
+    # case 2
+    @puzzle.input = '^>v<'
+    @puzzle.houses_visited = []
+    expected = [[0, 0],
+                [0 ,1],
+                [1, 1],
+                [1, 0],
+                [0, 0]]
+
+    @puzzle.travel
+    assert_equal expected, @puzzle.houses_visited,
+                 'Instructions incorrectly parsed'
+
+    # case 3
+    @puzzle.input = '^v^v^v^v^v'
+    @puzzle.houses_visited = []
+    expected = [[0, 0],
+                [0, 1],
+                [0, 0],
+                [0, 1],
+                [0, 0],
+                [0, 1],
+                [0, 0],
+                [0, 1],
+                [0, 0],
+                [0, 1],
+                [0, 0]]
+
+    @puzzle.travel
+    assert_equal expected, @puzzle.houses_visited,
+                 'Instructions incorrectly parsed'
+  end
+
+  def test_counts_houses_visited_by_both
+    # case 1
+    @puzzle.input = '^v'
+    @puzzle.houses_visited_by_santa = []
+    @puzzle.houses_visited_by_robot = []
+
+    @puzzle.travel_both
+
+    assert_equal 3, @puzzle.houses_visited_by_both_count, 'Wrong output'
+
+    # case 2
+    @puzzle.input = '^>v<'
+    @puzzle.houses_visited_by_santa = []
+    @puzzle.houses_visited_by_robot = []
+
+    @puzzle.travel_both
+
+    assert_equal 3, @puzzle.houses_visited_by_both_count, 'Wrong output'
+
+    # case 3 
+    @puzzle.input = '^v^v^v^v^v'
+    @puzzle.houses_visited_by_santa = []
+    @puzzle.houses_visited_by_robot = []
+
+    @puzzle.travel_both
+
+    assert_equal 11, @puzzle.houses_visited_by_both_count, 'Wrong output'
+  end
+
+  def test_travel_both_parses_instructions
+        # case 1
+        @puzzle.input = '^v'
+        @puzzle.houses_visited_by_santa = []
+        @puzzle.houses_visited_by_robot = []
+        expected_santa = [[0, 0],
+                          [0 ,1]]
+        expected_robot = [[0, 0],
+                          [0 ,-1]]
+    
+        @puzzle.travel_both
+        assert_equal expected_santa, @puzzle.houses_visited_by_santa,
+                    'Instructions incorrectly parsed'
+        assert_equal expected_robot, @puzzle.houses_visited_by_robot,
+                    'Instructions incorrectly parsed'
+    
+        # case 2
+        @puzzle.input = '^>v<'
+        @puzzle.houses_visited_by_santa = []
+        @puzzle.houses_visited_by_robot = []
+        expected_santa = [[0, 0],
+                          [0 ,1],
+                          [0, 0]]
+        expected_robot =  [[0, 0],
+                           [1, 0],
+                           [0, 0]]
+    
+        @puzzle.travel_both
+        assert_equal expected_santa, @puzzle.houses_visited_by_santa,
+                    'Instructions incorrectly parsed'
+        assert_equal expected_robot, @puzzle.houses_visited_by_robot,
+                    'Instructions incorrectly parsed'
+    
+        # case 3
+        @puzzle.input = '^v^v^v^v^v'
+        @puzzle.houses_visited_by_santa = []
+        @puzzle.houses_visited_by_robot = []
+        expected_santa = [[0, 0],
+                          [0, 1],
+                          [0, 2],
+                          [0, 3],
+                          [0, 4],
+                          [0, 5]]
+        expected_robot = [[0, 0],
+                          [0, -1],
+                          [0, -2],
+                          [0, -3],
+                          [0, -4],
+                          [0, -5]]
+    
+        @puzzle.travel_both
+        assert_equal expected_santa, @puzzle.houses_visited_by_santa,
+                    'Instructions incorrectly parsed'
+        assert_equal expected_robot, @puzzle.houses_visited_by_robot,
+                    'Instructions incorrectly parsed'
+  end
 
 end
